9c584c529f6c5a397eeb746daab867e10bfa213e
ask <ask@ygi.li>
2024-05-27 19:51:45 +0530

    feat: add the c# project

---
+++ open-redirect-vulnerabilities/DemoExeFile/DemoExeFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", "hacked.txt");
+            File.WriteAllText(path, "You have been HACKED!!! by me");
+
+            Console.WriteLine("File created at: " + path);
+        }
+    }
+}
