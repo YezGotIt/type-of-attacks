ca80f4704af0a398dd2daf38a3a43423b90b504f
ask <ask@ygi.li>
2024-05-27 20:43:55 +0530

    chore: change to working method

---
+++ open-redirect-vulnerabilities/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", "hacked.txt");
+            File.WriteAllText(path, "You have been HACKED!!! by me");
+        }
+    }
+}
